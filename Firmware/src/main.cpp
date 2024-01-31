8427b640678d58b81c54a4decf89e9bb599c9e86
jncronin <jncronin@tysos.org>
2024-01-31 15:41:26 +0000

    Switch to using HSI as PLL source for now

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -18,37 +18,9 @@ int main()
     s.Schedule(Thread::Create("idle_cm7", idle_thread, (void*)0, true, 0, CPUAffinity::M7Only, 512));
     s.Schedule(Thread::Create("idle_cm4", idle_thread, (void*)1, true, 0, CPUAffinity::M4Only, 512));
 
-    // Set MCO2 (PC9 AF0 = SDMMC1_D1), MCO1 = PA8
+    // Get systick to flash an LED for now (BTNLED_R = PC6)
     RCC->AHB4ENR |= RCC_AHB4ENR_GPIOAEN | RCC_AHB4ENR_GPIOCEN;
     (void)RCC->AHB4ENR;
-    {
-        auto gc = GPIOC->MODER;
-        gc &= ~(3UL << 18);
-        gc |= 2UL << 18;
-        GPIOC->MODER = gc;
-
-        auto af = GPIOC->AFR[1];
-        af &= ~(0xFUL << 4);
-        GPIOC->AFR[1] = af;
-
-        GPIOC->OSPEEDR |= (3UL << 18);
-
-        auto ga = GPIOA->MODER;
-        ga &= ~(3UL << 16);
-        ga |= 2UL << 16;
-        GPIOA->MODER = ga;
-
-        auto afa = GPIOA->AFR[1];
-        afa &= ~(0xFUL << 0);
-        GPIOA->AFR[1] = afa;
-
-        GPIOA->OSPEEDR |= (3UL << 16);
-    }
-    RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_MCO2_Msk) | (3UL << RCC_CFGR_MCO2_Pos);
-    RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_MCO1_Msk) | (2UL << RCC_CFGR_MCO1_Pos);
-    RCC->CFGR |= (10UL << RCC_CFGR_MCO2PRE_Pos) | (10UL << RCC_CFGR_MCO1PRE_Pos);
-
-    // Get systick to flash an LED for now (BTNLED_R = PC6)
     {
         auto gc = GPIOC->MODER;
         gc &= ~(3UL << 12);
