24636118d0478878d9fbf45821a707d486ba3903
John Cronin <jncronin@tysos.org>
2024-02-05 11:04:25 +0000

    Queue fixes

--- Firmware/inc/osqueue.h
+++ Firmware/inc/osqueue.h
@@ -3,6 +3,7 @@
 
 #include "region_allocator.h"
 #include "osmutex.h"
+#include "thread.h"
 #include <queue>
 
 template <typename T> using SRAM4Queue = std::queue<T, std::deque<T, SRAM4RegionAllocator<T>>>;
@@ -12,54 +13,90 @@ template <typename T, int nitems> class FixedQueue
     protected:
         T _b[nitems];
         Spinlock sl;
-        int _n;
-        Condition waiting;
+        int _wptr = 0;
+        int _rptr = 0;
+        SRAM4Vector<Thread *> waiting_threads;
+
+        constexpr inline int ptr_plus_one(int p)
+        {
+            p++;
+            if(p >= nitems)
+                p = 0;
+            return p;
+        }
+
+        inline void signal_waiting()
+        {
+            auto t = GetCurrentThreadForCore();
+            bool hpt = false;
+            for(auto bt : waiting_threads)
+            {
+                bt->is_blocking = false;
+                if(bt->base_priority > t->base_priority)
+                    hpt = true;
+            }
+            waiting_threads.clear();
+            if(hpt)
+            {
+                SCB->ICSR = SCB_ICSR_PENDSVSET_Msk;
+            }
+        }
 
     public:
+        inline bool empty()
+        {
+            return _wptr == _rptr;
+        }
+
+        inline bool full()
+        {
+            return ptr_plus_one(_wptr) == _rptr;
+        }
+
         bool Push(const T& v)
         {
             CriticalGuard cg(sl);
-            if(_n >= nitems)
+            if(full())
                 return false;
-            _b[_n++] = v;
-            waiting.Signal();
+            _b[_wptr] = v;
+            _wptr = ptr_plus_one(_wptr);
+            signal_waiting();
             return true;
         }
 
         bool Peek(T *v)
         {
+            if(!v)
+                return false;
+
             CriticalGuard cg(sl);
-            if(_n)
-            {
-                *v = _b[_n - 1];
-                return true;
-            }
-            return false;
+            if(empty())
+                return false;
+            return _b[_rptr];
         }
 
         bool Pop(T *v)
         {
+            if(!v)
+                return false;
+            
+            while(true)
             {
                 CriticalGuard cg(sl);
-                if(_n)
+                if(empty())
                 {
-                    *v = _b[--_n];
-                    return true;
+                    auto t = GetCurrentThreadForCore();
+                    waiting_threads.push_back(t);
+                    t->is_blocking = true;
+                    SCB->ICSR = SCB_ICSR_PENDSVSET_Msk;
                 }
-
-                waiting.Wait();
-            }
-            while(!_n) {}
-
-            {
-                CriticalGuard cg(sl);
-                if(_n)
+                else
                 {
-                    *v = _b[--_n];
+                    *v = _b[_rptr];
+                    _rptr = ptr_plus_one(_rptr);
                     return true;
                 }
             }
-            return false;
         }
 };
 
