1530007c1bd71bae9e415c287afdcd3fdfcd1172
John Cronin <jncronin@tysos.org>
2024-02-20 09:07:53 +0000

    Make C-compatible queues

--- Firmware/inc/tusb_config.h
+++ Firmware/inc/tusb_config.h
@@ -49,7 +49,7 @@
 
 #define CFG_TUD_HID             0
 #define CFG_TUD_CDC             1
-#define CFG_TUD_MSC             1
+#define CFG_TUD_MSC             0
 #define CFG_TUD_MIDI            0
 #define CFG_TUD_VENDOR          0
 #define CFG_TUD_AUDIO           0
--- Firmware/inc/tusb_os_custom.h
+++ Firmware/inc/tusb_os_custom.h
@@ -12,15 +12,10 @@ typedef struct
 {
     uint16_t depth;
     uint16_t item_sz;
-    void*    buf;
-    char const* name;
-    uint32_t q;
 } osal_queue_def_t;
 
-#define _OSAL_Q_NAME(_name) .name = #_name
 #define OSAL_QUEUE_DEF(_int_set, _name, _depth, _type) \
-    static _type _name##_##buf[_depth];\
-    osal_queue_def_t _name = { .depth = _depth, .item_sz = sizeof(_type), .buf = _name##_##buf, _OSAL_Q_NAME(_name) };
+    osal_queue_def_t _name = { .depth = _depth, .item_sz = sizeof(_type) };
 
 
 /* C interface to the C++ Spinlock class */
@@ -28,14 +23,6 @@ typedef void * osal_mutex_t;
 typedef void * osal_mutex_def_t;
 
 typedef void * osal_queue_t;
-
-typedef void * osal_semaphore_t;
-typedef void * osal_semaphore_def_t;
-
-osal_semaphore_t osal_semaphore_create(osal_semaphore_def_t* semdef);
-bool osal_semaphore_post(osal_semaphore_t sem_hdl, bool in_isr);
-bool osal_semaphore_wait(osal_semaphore_t sem_hdl, uint32_t msec);
-void osal_semaphore_reset(osal_semaphore_t sem_hdl); // TODO removed
 
 osal_mutex_t osal_mutex_create(osal_mutex_def_t* mdef);
 bool osal_mutex_lock (osal_mutex_t sem_hdl, uint32_t msec);
