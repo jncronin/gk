12ba0ab6bcdedaf02059cd48c60e7ceff3024001
John Cronin <jncronin@tysos.org>
2024-02-24 17:41:46 +0000

    break on unhandled syscall

--- Firmware/src/syscalls.cpp
+++ Firmware/src/syscalls.cpp
@@ -200,6 +200,7 @@ void SyscallHandler(syscall_no sno, void *r1, void *r2, void *r3)
             break;
 
         default:
+            __asm__ volatile ("bkpt #0\n");
             while(true);
             break;
     }
