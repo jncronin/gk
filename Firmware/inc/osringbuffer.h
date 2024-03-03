60e44e9d1c3be8caf89e2f99c88076db35a4cce2
John Cronin <jncronin@tysos.org>
2024-03-03 14:05:44 +0000

    Add ringbuffer support for types that are not copy-assignable

--- Firmware/inc/osringbuffer.h
+++ Firmware/inc/osringbuffer.h
@@ -3,11 +3,15 @@
 
 #include <stddef.h>
 #include <cstring>
+#include <utility>
+#include <type_traits>
 
 size_t memcpy_split_src(void *dst, const void *src, size_t n, size_t src_ptr, size_t split_ptr);
 size_t memcpy_split_dest(void *dst, const void *src, size_t n, size_t dest_ptr, size_t split_ptr);
 
-template<typename T, unsigned int nitems> class RingBuffer
+template<typename T, unsigned int nitems, bool = std::is_copy_assignable<T>::value> class RingBuffer;
+
+template<typename T, unsigned int nitems> class RingBuffer<T, nitems, true>
 {
     protected:
         size_t _wptr = 0;
@@ -46,6 +50,7 @@ template<typename T, unsigned int nitems> class RingBuffer
             }
             return nwritten;
         }
+
         int Write(const T &d)
         {
             return Write(&d);
@@ -60,11 +65,73 @@ template<typename T, unsigned int nitems> class RingBuffer
                 {
                     return nread;
                 }
-                d[i] = b[_rptr];
+                d[i] = std::move(b[_rptr]);
                 _rptr = ptr_plus_one(_rptr);
             }
             return nread;
         }
 };
+
+template<typename T, unsigned int nitems> class RingBuffer<T, nitems, false>
+{
+    protected:
+        size_t _wptr = 0;
+        size_t _rptr = 0;
+        T b[nitems];
+        constexpr inline unsigned int ptr_plus_one(unsigned int p)
+        {
+            p++;
+            if(p >= nitems)
+                p = 0;
+            return p;
+        }
+
+    public:        
+        inline bool empty()
+        {
+            return _wptr == _rptr;
+        }
+
+        inline bool full()
+        {
+            return ptr_plus_one(_wptr) == _rptr;
+        }
+
+        int Write(T* d, int n = 1)
+        {
+            int nwritten = 0;
+            for(int i = 0; i < n; i++, nwritten++)
+            {
+                if(full())
+                {
+                    return nwritten;
+                }
+                b[_wptr] = std::move(d[i]);
+                _wptr = ptr_plus_one(_wptr);
+            }
+            return nwritten;
+        }
+
+        int Write(T &&d)
+        {
+            return Write(&d);
+        }
+
+        int Read(T* d, int n = 1)
+        {
+            int nread = 0;
+            for(int i = 0; i < n; i++, nread++)
+            {
+                if(empty())
+                {
+                    return nread;
+                }
+                d[i] = std::move(b[_rptr]);
+                _rptr = ptr_plus_one(_rptr);
+            }
+            return nread;
+        }
+};
+
 
 #endif
