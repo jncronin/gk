7f4f8c4cc6dc5ba374427904e377958b71fff07f
John Cronin <jncronin@tysos.org>
2024-03-24 17:48:15 +0000

    usb cdc fixes to get echo working

--- Firmware/inc/gk_conf.h
+++ Firmware/inc/gk_conf.h
@@ -6,6 +6,7 @@
 #define GK_ENABLE_USB_MASS_STORAGE  1
 #define GK_ENABLE_LWEXT4_WRITE      0
 #define GK_ENABLE_TEST_THREADS      0
+#define GK_USE_IRQ_PRIORITIES       0
 #define GK_DUAL_CORE                0
 #define GK_USE_CACHE                0
 #define GK_EXT_READONLY             0
--- Firmware/inc/util.h
+++ Firmware/inc/util.h
@@ -6,14 +6,23 @@
 
 inline __attribute__((always_inline)) static uint32_t DisableInterrupts()
 {
+#if GK_USE_IRQ_PRIORITIES
     auto cpsr = __get_BASEPRI();
     __set_BASEPRI(0x10U);
+#else
+    auto cpsr = __get_PRIMASK();
+    __disable_irq();
+#endif
     return cpsr;
 }
 
 inline __attribute__((always_inline)) static void RestoreInterrupts(uint32_t cpsr)
 {
+#if GK_USE_IRQ_PRIORITIES
     __set_BASEPRI(cpsr);
+#else
+    __set_PRIMASK(cpsr);
+#endif
 }
 
 /* Need to use HSEM here because STM32H7 does not implement bus locking on AXI */
