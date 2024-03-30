e35d86a9846a82b5319f26b4f65512924d5ec418
John Cronin <jncronin@tysos.org>
2024-03-30 12:55:38 +0000

    disable dual core for now

--- Firmware/inc/gk_conf.h
+++ Firmware/inc/gk_conf.h
@@ -7,7 +7,7 @@
 #define GK_ENABLE_LWEXT4_WRITE      0
 #define GK_ENABLE_TEST_THREADS      0
 #define GK_USE_IRQ_PRIORITIES       0
-#define GK_DUAL_CORE                1
+#define GK_DUAL_CORE                0
 #define GK_USE_CACHE                1
 #define GK_USE_MPU                  1
 #define GK_EXT_READONLY             0
