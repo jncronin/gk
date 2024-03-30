e47c6c3831342e7a2fadd0e4ac2a4e27eb50f21e
John Cronin <jncronin@tysos.org>
2024-03-30 18:57:03 +0000

    gpu debugging

--- Firmware/src/gpu.cpp
+++ Firmware/src/gpu.cpp
@@ -10,7 +10,14 @@ __attribute__((section (".sram4"))) static FixedQueue<gpu_message, 8> gpu_msg_li
 extern Spinlock s_rtt;
 #include "SEGGER_RTT.h"
 
-#define GPU_DEBUG 0
+extern Condition scr_vsync;
+
+#define GPU_DEBUG 1
+
+static inline void wait_dma2d()
+{
+    while(DMA2D->CR & DMA2D_CR_START);
+}
 
 void *gpu_thread(void *p)
 {
@@ -27,25 +34,39 @@ void *gpu_thread(void *p)
         if(!gpu_msg_list.Pop(&g))
             continue;
 
+        auto ltdc_curfb = LTDC_Layer1->CFBAR;
+        auto scr_curfb = (uint32_t)(uintptr_t)screen_get_frame_buffer();
+
 #if GPU_DEBUG
         {
             CriticalGuard cg(s_rtt);
             SEGGER_RTT_printf(0, "gpu: @%u type: %d, dest_addr: %x, src_addr_color: %x, nlines: %x, row_width: %x\n",
                 (unsigned int)clock_cur_ms(), g.type, g.dest_addr, g.src_addr_color, g.nlines, g.row_width);
+            SEGGER_RTT_printf(0, "gpu: ltdc_fb: %x, scr_fb: %x\n", ltdc_curfb, scr_curfb);
         }
 #endif
+
+        // We should never be able to write to the current framebuffer
+        {
+            while(LTDC_Layer1->CFBAR == (uint32_t)(uintptr_t)screen_get_frame_buffer())
+                Yield();
+        }
         
         switch(g.type)
         {
             case gpu_message_type::FlipBuffers:
+                wait_dma2d();
                 screen_flip();
+                scr_vsync.Wait();
                 break;
 
             case gpu_message_type::SetBuffers:
+                wait_dma2d();
                 screen_set_frame_buffer((void *)g.dest_addr, (void*)g.src_addr_color, g.dest_pf);
                 break;
 
             case gpu_message_type::CleanCache:
+                wait_dma2d();
                 {
                     auto start_addr = g.dest_addr + (g.dest_fbuf_relative ? (uint32_t)(uintptr_t)screen_get_frame_buffer() : 0UL);
                     auto len = g.row_width * 4 + g.nlines * 640 * 4;
@@ -54,6 +75,7 @@ void *gpu_thread(void *p)
                 break;
 
             case gpu_message_type::SignalThread:
+                wait_dma2d();
                 {
                     auto t = (Thread *)g.dest_addr;
                     {
@@ -67,6 +89,7 @@ void *gpu_thread(void *p)
             case gpu_message_type::BlitColor:
                 if(!g.row_width || !g.nlines)
                     break;
+                wait_dma2d();
                 DMA2D->OPFCCR = g.dest_pf;
                 DMA2D->OCOLR = g.src_addr_color;
                 DMA2D->OMAR = g.dest_addr + (g.dest_fbuf_relative ? (uint32_t)(uintptr_t)screen_get_frame_buffer() : 0UL);
@@ -81,6 +104,7 @@ void *gpu_thread(void *p)
             case gpu_message_type::BlitImage:
                 if(!g.row_width || !g.nlines)
                     break;
+                wait_dma2d();
                 DMA2D->OPFCCR = g.dest_pf;
                 DMA2D->OMAR = g.dest_addr + (g.dest_fbuf_relative ? (uint32_t)(uintptr_t)screen_get_frame_buffer() : 0UL);
                 DMA2D->OOR = 640 - g.row_width;
@@ -158,6 +182,6 @@ extern "C" void DMA2D_IRQHandler()
 #if GPU_DEBUG
     SEGGER_RTT_printf(0, "gpuint: @%u\n", clock_cur_ms());
 #endif
-    gpu_ready.Signal();
     DMA2D->IFCR = DMA2D_IFCR_CTCIF | DMA2D_IFCR_CTEIF;
+    gpu_ready.Signal();
 }
