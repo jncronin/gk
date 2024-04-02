325d13069dc0e757b1a5cc05347d31b4f402e7c8
John Cronin <jncronin@tysos.org>
2024-04-02 17:32:01 +0100

    Allow changing background in ClearScreen method

--- Firmware/src/gpu.cpp
+++ Firmware/src/gpu.cpp
@@ -12,6 +12,8 @@ extern Spinlock s_rtt;
 
 extern Condition scr_vsync;
 
+SRAM4_DATA gpu_message gpu_clear_screen_msg { .type = ClearScreen };
+
 #define GPU_DEBUG 0
 
 static inline void wait_dma2d()
@@ -72,6 +74,26 @@ static inline uint32_t color_encode(uint32_t col, uint32_t pf)
     }
 }
 
+static void handle_clearscreen(const gpu_message &curmsg, gpu_message *newmsgs, size_t *nnewmsgs)
+{
+    if(gpu_clear_screen_msg.type == ClearScreen)
+    {
+        // init message
+        gpu_clear_screen_msg.dest_addr = 0;
+        gpu_clear_screen_msg.dx = 0;
+        gpu_clear_screen_msg.dy = 0;
+        gpu_clear_screen_msg.w = 640;
+        gpu_clear_screen_msg.h = 480;
+        gpu_clear_screen_msg.src_addr_color = 0U;
+        gpu_clear_screen_msg.type = BlitColor;
+    }
+    newmsgs[0] = gpu_clear_screen_msg;
+    if(gpu_clear_screen_msg.type == BlitColor)
+        newmsgs[0].src_addr_color = curmsg.src_addr_color;
+
+    *nnewmsgs = 1;
+}
+
 void *gpu_thread(void *p)
 {
     (void)p;
@@ -83,129 +105,150 @@ void *gpu_thread(void *p)
 
     while(true)
     {
-        gpu_message g;
-        if(!gpu_msg_list.Pop(&g))
+        gpu_message curmsgs[8];
+        if(!gpu_msg_list.Pop(&curmsgs[0]))
             continue;
 
+        // Some messages decompose to more than one - handle this here
+        size_t nmsgs = sizeof(curmsgs) / sizeof(gpu_message);
 
-#if GPU_DEBUG
-        auto ltdc_curfb = LTDC_Layer1->CFBAR;
-        auto scr_curfb = (uint32_t)(uintptr_t)screen_get_frame_buffer();
+        // Handle messages which decompose to others
+        switch(curmsgs[0].type)
         {
-            CriticalGuard cg(s_rtt);
-            SEGGER_RTT_printf(0, "gpu: @%u type: %d, dest_addr: %x, src_addr_color: %x, nlines: %x, row_width: %x\n",
-                (unsigned int)clock_cur_ms(), g.type, g.dest_addr, g.src_addr_color, g.h, g.w);
-            SEGGER_RTT_printf(0, "gpu: ltdc_fb: %x, scr_fb: %x\n", ltdc_curfb, scr_curfb);
+            case ClearScreen:
+                handle_clearscreen(curmsgs[0], curmsgs, &nmsgs);
+                break;
+            default:
+                nmsgs = 1;
+                break;
         }
+
+        for(size_t i = 0; i < nmsgs; i++)
+        {
+            auto &g = curmsgs[i];
+#if GPU_DEBUG
+            auto ltdc_curfb = LTDC_Layer1->CFBAR;
+            auto scr_curfb = (uint32_t)(uintptr_t)screen_get_frame_buffer();
+            {
+                CriticalGuard cg(s_rtt);
+                SEGGER_RTT_printf(0, "gpu: @%u type: %d, dest_addr: %x, src_addr_color: %x, nlines: %x, row_width: %x\n",
+                    (unsigned int)clock_cur_ms(), g.type, g.dest_addr, g.src_addr_color, g.h, g.w);
+                SEGGER_RTT_printf(0, "gpu: ltdc_fb: %x, scr_fb: %x\n", ltdc_curfb, scr_curfb);
+            }
 #endif
 
-        // We should never be able to write to the current framebuffer
-        {
-            while(LTDC_Layer1->CFBAR == (uint32_t)(uintptr_t)screen_get_frame_buffer())
-                Yield();
-        }
+            // We should never be able to write to the current framebuffer
+            {
+                while(LTDC_Layer1->CFBAR == (uint32_t)(uintptr_t)screen_get_frame_buffer())
+                    Yield();
+            }
 
-        /* get details on pixel formats, strides etc */
-        uint32_t dest_pf = g.dest_addr ? g.dest_pf : focus_process->screen_mode;
-        int bpp = get_bpp(dest_pf);
-        //uint32_t scr_fbuf = (uint32_t)(uintptr_t)screen_get_frame_buffer();
-        uint32_t dest_addr = g.dest_addr ? g.dest_addr : (uint32_t)(uintptr_t)screen_get_frame_buffer();
-        uint32_t dest_pitch = g.dest_addr ? g.dp : 640 * bpp;
+            /* get details on pixel formats, strides etc */
+            uint32_t dest_pf = g.dest_addr ? g.dest_pf : focus_process->screen_mode;
+            int bpp = get_bpp(dest_pf);
+            //uint32_t scr_fbuf = (uint32_t)(uintptr_t)screen_get_frame_buffer();
+            uint32_t dest_addr = g.dest_addr ? g.dest_addr : (uint32_t)(uintptr_t)screen_get_frame_buffer();
+            uint32_t dest_pitch = g.dest_addr ? g.dp : 640 * bpp;
 
 
-        
-        switch(g.type)
-        {
-            case gpu_message_type::FlipBuffers:
-                wait_dma2d();
-                screen_flip();
-                scr_vsync.Wait();
-                break;
-
-            case gpu_message_type::SetBuffers:
-                screen_set_frame_buffer((void *)g.dest_addr, (void*)g.src_addr_color);
-                break;
-
-            case gpu_message_type::CleanCache:
-                {
-                    auto start_addr = dest_addr + g.dx * bpp + g.dy * dest_pitch;
-                    auto len = g.h * dest_pitch + g.w * bpp;
-                    CleanM7Cache(start_addr, len, CacheType_t::Data);
-                }
-                break;
-
-            case gpu_message_type::SignalThread:
-                wait_dma2d();
-                {
-                    auto t = (Thread *)g.dest_addr;
-                    {
-                        CriticalGuard cg(t->sl);
-                        t->ss_p.ival1 = 0;
-                        t->ss.Signal();
-                    }
-                }
-                break;
-
-            case gpu_message_type::BlitColor:
-                if(!g.w || !g.h)
+            
+            switch(g.type)
+            {
+                case gpu_message_type::FlipBuffers:
+                    wait_dma2d();
+                    screen_flip();
+                    scr_vsync.Wait();
                     break;
-                wait_dma2d();
-                DMA2D->OPFCCR = dest_pf;
-                DMA2D->OCOLR = color_encode(g.src_addr_color, dest_pf);
-                DMA2D->OMAR = dest_addr + g.dx * bpp + g.dy * dest_pitch;
-                DMA2D->OOR = (dest_pitch / bpp) - g.w;
-                DMA2D->NLR = (g.w << DMA2D_NLR_PL_Pos) | (g.h << DMA2D_NLR_NL_Pos);
-                DMA2D->CR = DMA2D_CR_TCIE |
-                    DMA2D_CR_TEIE |
-                    (3UL << DMA2D_CR_MODE_Pos) | DMA2D_CR_START;
-                break;
 
-            case gpu_message_type::BlitImage:
-                if(!g.w || !g.h)
+                case gpu_message_type::SetBuffers:
+                    screen_set_frame_buffer((void *)g.dest_addr, (void*)g.src_addr_color);
                     break;
-                
-                wait_dma2d();
-                DMA2D->OPFCCR = dest_pf;
-                DMA2D->OMAR = dest_addr + g.dx * bpp + g.dy * dest_pitch;
-                DMA2D->OOR = (dest_pitch / bpp) - g.w;
-                DMA2D->NLR = (g.w << DMA2D_NLR_PL_Pos) | (g.h << DMA2D_NLR_NL_Pos);
 
-                {
-                    // configure source, +/- pixel format correction
-                    auto src_bpp = get_bpp(g.src_pf);
-                    DMA2D->FGMAR = g.src_addr_color + g.sx * src_bpp + g.sy * g.sp;
-                    DMA2D->FGPFCCR = g.src_pf;
-                    DMA2D->FGOR = (g.sp / src_bpp) - g.w;
+                case gpu_message_type::CleanCache:
+                    {
+                        auto start_addr = dest_addr + g.dx * bpp + g.dy * dest_pitch;
+                        auto len = g.h * dest_pitch + g.w * bpp;
+                        CleanM7Cache(start_addr, len, CacheType_t::Data);
+                    }
+                    break;
 
-                    // does it blend?
-                    uint32_t mode = 0;
-                    if(g.src_pf == GK_PIXELFORMAT_ARGB8888)
+                case gpu_message_type::SignalThread:
+                    wait_dma2d();
                     {
-                        mode = 2U;
-                        
-                        // set background as scratch to allow blend
-                        DMA2D->BGMAR = dest_addr + g.dx * bpp + g.dy * dest_pitch;
-                        DMA2D->BGOR = (dest_pitch / bpp) - g.w;
-                        DMA2D->BGPFCCR = dest_pf;
+                        auto t = (Thread *)g.dest_addr;
+                        {
+                            CriticalGuard cg(t->sl);
+                            t->ss_p.ival1 = 0;
+                            t->ss.Signal();
+                        }
                     }
-                    else if(g.src_pf != dest_pf)
-                    {
-                        // need pixel format conversion, but no blending
-                        mode = 1U;
-                    }
-                    DMA2D->CR = DMA2D_CR_TCIE | 
+                    break;
+
+                case gpu_message_type::BlitColor:
+                    if(!g.w || !g.h)
+                        break;
+                    wait_dma2d();
+                    DMA2D->OPFCCR = dest_pf;
+                    DMA2D->OCOLR = color_encode(g.src_addr_color, dest_pf);
+                    DMA2D->OMAR = dest_addr + g.dx * bpp + g.dy * dest_pitch;
+                    DMA2D->OOR = (dest_pitch / bpp) - g.w;
+                    DMA2D->NLR = (g.w << DMA2D_NLR_PL_Pos) | (g.h << DMA2D_NLR_NL_Pos);
+                    DMA2D->CR = DMA2D_CR_TCIE |
                         DMA2D_CR_TEIE |
-                        (mode << DMA2D_CR_MODE_Pos) | DMA2D_CR_START;
-                }
-                break;
-        }
+                        (3UL << DMA2D_CR_MODE_Pos) | DMA2D_CR_START;
+                    break;
+
+                case gpu_message_type::BlitImage:
+                    if(!g.w || !g.h)
+                        break;
+                    
+                    wait_dma2d();
+                    DMA2D->OPFCCR = dest_pf;
+                    DMA2D->OMAR = dest_addr + g.dx * bpp + g.dy * dest_pitch;
+                    DMA2D->OOR = (dest_pitch / bpp) - g.w;
+                    DMA2D->NLR = (g.w << DMA2D_NLR_PL_Pos) | (g.h << DMA2D_NLR_NL_Pos);
+
+                    {
+                        // configure source, +/- pixel format correction
+                        auto src_bpp = get_bpp(g.src_pf);
+                        DMA2D->FGMAR = g.src_addr_color + g.sx * src_bpp + g.sy * g.sp;
+                        DMA2D->FGPFCCR = g.src_pf;
+                        DMA2D->FGOR = (g.sp / src_bpp) - g.w;
+
+                        // does it blend?
+                        uint32_t mode = 0;
+                        if(g.src_pf == GK_PIXELFORMAT_ARGB8888)
+                        {
+                            mode = 2U;
+                            
+                            // set background as scratch to allow blend
+                            DMA2D->BGMAR = dest_addr + g.dx * bpp + g.dy * dest_pitch;
+                            DMA2D->BGOR = (dest_pitch / bpp) - g.w;
+                            DMA2D->BGPFCCR = dest_pf;
+                        }
+                        else if(g.src_pf != dest_pf)
+                        {
+                            // need pixel format conversion, but no blending
+                            mode = 1U;
+                        }
+                        DMA2D->CR = DMA2D_CR_TCIE | 
+                            DMA2D_CR_TEIE |
+                            (mode << DMA2D_CR_MODE_Pos) | DMA2D_CR_START;
+                    }
+                    break;
+
+                case ClearScreen:
+                    // shouldn't get here
+                    break;
+            }
 #if GPU_DEBUG
-        {
-            CriticalGuard cg(s_rtt);
-            SEGGER_RTT_printf(0, "gpu: @%u complete\n",
-                (unsigned int)clock_cur_ms());
-        }
+            {
+                CriticalGuard cg(s_rtt);
+                SEGGER_RTT_printf(0, "gpu: @%u complete\n",
+                    (unsigned int)clock_cur_ms());
+            }
 #endif
+        }
 
     }
 }
