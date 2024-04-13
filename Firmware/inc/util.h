920960bf8d4c8c886126af5cb3e8210e19af799b
John Cronin <jncronin@tysos.org>
2024-04-13 10:51:55 +0100

    split hsem accesses based on ptr address

--- Firmware/inc/util.h
+++ Firmware/inc/util.h
@@ -25,10 +25,19 @@ inline __attribute__((always_inline)) static void RestoreInterrupts(uint32_t cps
 #endif
 }
 
+static inline unsigned int ptr_to_hsem_id(volatile void* ptr)
+{
+    /* split hsem accesses so we can use more than one
+        need to profile to determine the best algorithm here but at a guess this
+        seems okay */
+    unsigned int ret = (unsigned int)(uintptr_t)ptr;
+    return (ret >> 2) & 0x1fU;
+}
+
 /* Need to use HSEM here because STM32H7 does not implement bus locking on AXI */
 template <typename T> static inline void cmpxchg(volatile T* ptr, T* oldval, T newval)
 {
-    while(HSEM->RLR[0] == 0);
+    while(HSEM->RLR[ptr_to_hsem_id(ptr)] == 0);
     if(*ptr == *oldval)
     {
         *ptr = newval;
@@ -37,14 +46,14 @@ template <typename T> static inline void cmpxchg(volatile T* ptr, T* oldval, T n
     {
         *oldval = *ptr;
     }
-    HSEM->R[0] = 0;
+    HSEM->R[ptr_to_hsem_id(ptr)] = 0;
 }
 
 template <typename T> static inline void set(volatile T* ptr, T newval)
 {
-    while(HSEM->RLR[0] == 0);
+    while(HSEM->RLR[ptr_to_hsem_id(ptr)] == 0);
     *ptr = newval;
-    HSEM->R[0] = 0;
+    HSEM->R[ptr_to_hsem_id(ptr)] = 0;
 }
 
 #endif
