d9eabc6f5f5edb642df09d884eb6cbe091e57312
John Cronin <jncronin@tysos.org>
2024-05-04 19:02:07 +0100

    add button debounce

--- Firmware/src/buttons.cpp
+++ Firmware/src/buttons.cpp
@@ -1,6 +1,7 @@
 #include "stm32h7xx.h"
 #include "pins.h"
 #include "supervisor.h"
+#include "debounce.h"
 
 constexpr const pin BTN_MENU { GPIOB, 4 };
 constexpr const pin BTN_VOLUP { GPIOB, 11 };
@@ -14,6 +15,19 @@ constexpr const pin BTN_A { GPIOH, 9 };
 constexpr const pin BTN_B { GPIOH, 10 };
 constexpr const pin BTN_X { GPIOH, 13 };
 constexpr const pin BTN_Y { GPIOH, 15 };
+
+static Debounce db_MENU(BTN_MENU),
+    db_VOLUP(BTN_VOLUP),
+    db_VOLDOWN(BTN_VOLDOWN),
+    db_ONOFF(BTN_ONOFF),
+    db_L(BTN_L),
+    db_R(BTN_R),
+    db_U(BTN_U),
+    db_D(BTN_D),
+    db_A(BTN_A),
+    db_B(BTN_B),
+    db_X(BTN_X),
+    db_Y(BTN_Y);
 
 void init_buttons()
 {
@@ -30,36 +44,21 @@ void init_buttons()
     BTN_X.set_as_input();
     BTN_Y.set_as_input();
 
-    RCC->APB4ENR |= RCC_APB4ENR_SYSCFGEN;
+    // Set up lptim2 for debouncing
+    RCC->APB4ENR |= RCC_APB4ENR_LPTIM2EN;
     (void)RCC->APB4ENR;
+    LPTIM2->CR = 0;
+    LPTIM2->CR = LPTIM_CR_RSTARE;
+    (void)LPTIM2->CR;
+    LPTIM2->CR = 0;
 
-    SYSCFG->EXTICR[1] = (SYSCFG->EXTICR[4] & ~SYSCFG_EXTICR2_EXTI4_Msk) | SYSCFG_EXTICR2_EXTI4_PB;
-    SYSCFG->EXTICR[2] = (SYSCFG->EXTICR[2] & ~SYSCFG_EXTICR3_EXTI11_Msk) | SYSCFG_EXTICR3_EXTI11_PB;
-    SYSCFG->EXTICR[3] = (SYSCFG->EXTICR[3] & ~SYSCFG_EXTICR4_EXTI12_Msk) | SYSCFG_EXTICR4_EXTI12_PB;
-    SYSCFG->EXTICR[0] = (SYSCFG->EXTICR[0] & ~SYSCFG_EXTICR1_EXTI3_Msk) | SYSCFG_EXTICR1_EXTI3_PH;
-    SYSCFG->EXTICR[1] = (SYSCFG->EXTICR[1] & ~SYSCFG_EXTICR2_EXTI5_Msk) | SYSCFG_EXTICR2_EXTI5_PH;
-    SYSCFG->EXTICR[1] = (SYSCFG->EXTICR[1] & ~SYSCFG_EXTICR2_EXTI6_Msk) | SYSCFG_EXTICR2_EXTI6_PH;
-    SYSCFG->EXTICR[1] = (SYSCFG->EXTICR[1] & ~SYSCFG_EXTICR2_EXTI7_Msk) | SYSCFG_EXTICR2_EXTI7_PH;
-    SYSCFG->EXTICR[2] = (SYSCFG->EXTICR[2] & ~SYSCFG_EXTICR3_EXTI8_Msk) | SYSCFG_EXTICR3_EXTI8_PH;
-    SYSCFG->EXTICR[2] = (SYSCFG->EXTICR[2] & ~SYSCFG_EXTICR3_EXTI9_Msk) | SYSCFG_EXTICR3_EXTI9_PH;
-    SYSCFG->EXTICR[2] = (SYSCFG->EXTICR[2] & ~SYSCFG_EXTICR3_EXTI10_Msk) | SYSCFG_EXTICR3_EXTI10_PH;
-    SYSCFG->EXTICR[3] = (SYSCFG->EXTICR[3] & ~SYSCFG_EXTICR4_EXTI13_Msk) | SYSCFG_EXTICR4_EXTI13_PH;
-    SYSCFG->EXTICR[3] = (SYSCFG->EXTICR[3] & ~SYSCFG_EXTICR4_EXTI15_Msk) | SYSCFG_EXTICR4_EXTI15_PH;
+    LPTIM2->CFGR = 4UL << LPTIM_CFGR_PRESC_Pos;     // /16 => 1 MHz tick
+    LPTIM2->IER = LPTIM_IER_ARRMIE;
+    LPTIM2->CR = LPTIM_CR_ENABLE;
+    LPTIM2->ARR = 4999;                              // Reload every 5 ms
 
-    EXTI->FTSR1 |= (1U << 4) | (1U << 11) | (1U << 12) | (1U << 3) |
-        (1U << 5) | (1U << 6) | (1U << 7) | (1U << 8) |
-        (1U << 9) | (1U << 10) | (1U << 13) | (1U << 15);
-    EXTI->RTSR1 |= (1U << 4) | (1U << 11) | (1U << 12) | (1U << 3) |
-        (1U << 5) | (1U << 6) | (1U << 7) | (1U << 8) |
-        (1U << 9) | (1U << 10) | (1U << 13) | (1U << 15);
-    EXTI->IMR1 |= (1U << 4) | (1U << 11) | (1U << 12) | (1U << 3) |
-        (1U << 5) | (1U << 6) | (1U << 7) | (1U << 8) |
-        (1U << 9) | (1U << 10) | (1U << 13) | (1U << 15);
-
-    NVIC_EnableIRQ(EXTI4_IRQn);
-    NVIC_EnableIRQ(EXTI15_10_IRQn);
-    NVIC_EnableIRQ(EXTI3_IRQn);
-    NVIC_EnableIRQ(EXTI9_5_IRQn);
+    NVIC_EnableIRQ(LPTIM2_IRQn);
+    LPTIM2->CR = LPTIM_CR_ENABLE | LPTIM_CR_CNTSTRT;
 }
 
 static inline Process &recv_proc()
@@ -76,7 +75,6 @@ static inline Process &recv_proc()
 
 static inline void handle_state_change(int exti, bool pressed)
 {
-    // TODO: debounce
     Event::event_type_t et = pressed ? Event::KeyDown : Event::KeyUp;
 
     switch(exti)
@@ -125,87 +123,32 @@ static inline void handle_state_change(const pin &p)
     handle_state_change(p.pin, !p.value());
 }
 
-extern "C" void EXTI4_IRQHandler()
+template<int tick_period_ms,
+    int high_time_ms,
+    int low_time_ms,
+    int long_press_ms> void handle_debounce_event(Debounce<tick_period_ms, high_time_ms, low_time_ms, long_press_ms> &db)
 {
-    handle_state_change(BTN_MENU);
-    EXTI->PR1 = EXTI_PR1_PR4;
-    __DMB();
+    auto v = db.tick();
+    if(v & pin_state::NewStableState)
+    {
+        handle_state_change(db.get_pin().pin, v & pin_state::StableLow);
+    }
 }
 
-extern "C" void EXTI15_10_IRQHandler()
+extern "C" void LPTIM2_IRQHandler()
 {
-    uint32_t clear = 0;
-    uint32_t pr1 = EXTI->PR1;
-    if(pr1 & EXTI_PR1_PR10)
-    {
-        handle_state_change(BTN_B);
-        clear |= EXTI_PR1_PR10;
-    }
-    if(pr1 & EXTI_PR1_PR11)
-    {
-        handle_state_change(BTN_VOLUP);
-        clear |= EXTI_PR1_PR11;
-    }
-    if(pr1 & EXTI_PR1_PR12)
-    {
-        handle_state_change(BTN_VOLDOWN);
-        clear |= EXTI_PR1_PR12;
-    }
-    if(pr1 & EXTI_PR1_PR13)
-    {
-        handle_state_change(BTN_X);
-        clear |= EXTI_PR1_PR13;
-    }
-    if(pr1 & EXTI_PR1_PR14)
-    {
-        // CTP interrupt - TODO: send to ctp thread
-        clear |= EXTI_PR1_PR14;
-    }
-    if(pr1 & EXTI_PR1_PR15)
-    {
-        handle_state_change(BTN_Y);
-        clear |= EXTI_PR1_PR15;
-    }
-    EXTI->PR1 = clear;
-    __DMB();
-}
+    handle_debounce_event(db_MENU);
+    handle_debounce_event(db_VOLUP);
+    handle_debounce_event(db_VOLDOWN);
+    handle_debounce_event(db_L);
+    handle_debounce_event(db_R);
+    handle_debounce_event(db_U);
+    handle_debounce_event(db_D);
+    handle_debounce_event(db_A);
+    handle_debounce_event(db_B);
+    handle_debounce_event(db_X);
+    handle_debounce_event(db_Y);
 
-extern "C" void EXTI9_5_IRQHandler()
-{
-    uint32_t clear = 0;
-    uint32_t pr1 = EXTI->PR1;
-    if(pr1 & EXTI_PR1_PR5)
-    {
-        handle_state_change(BTN_L);
-        clear |= EXTI_PR1_PR5;
-    }
-    if(pr1 & EXTI_PR1_PR6)
-    {
-        handle_state_change(BTN_R);
-        clear |= EXTI_PR1_PR6;
-    }
-    if(pr1 & EXTI_PR1_PR7)
-    {
-        handle_state_change(BTN_U);
-        clear |= EXTI_PR1_PR7;
-    }
-    if(pr1 & EXTI_PR1_PR8)
-    {
-        handle_state_change(BTN_D);
-        clear |= EXTI_PR1_PR8;
-    }
-    if(pr1 & EXTI_PR1_PR9)
-    {
-        handle_state_change(BTN_A);
-        clear |= EXTI_PR1_PR9;
-    }
-    EXTI->PR1 = clear;
-    __DMB();    
-}
-
-extern "C" void EXTI3_IRQHandler()
-{
-    handle_state_change(BTN_ONOFF);
-    EXTI->PR1 = EXTI_PR1_PR3;
+    LPTIM2->ICR = LPTIM_ICR_ARRMCF;
     __DMB();
 }
