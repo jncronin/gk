b8c4276b2234bff397e1d7beb89253b5f5a3b8b6
John Cronin <jncronin@tysos.org>
2024-08-24 11:55:18 +0100

    revert to own widget system for supervisor

--- Firmware/src/supervisor/supervisor.cpp
+++ Firmware/src/supervisor/supervisor.cpp
@@ -4,37 +4,26 @@
 #include "screen.h"
 #include "cache.h"
 #include "_gk_event.h"
+#include "widgets/widget.h"
 #include "btnled.h"
 #include "brightness.h"
 #include "sound.h"
-#include "lvgl.h"
-#include "lv_drivers/lv_gk_display.h"
 #include "gk_conf.h"
-#include "lv_drivers/lv_gk_input.h"
 
 SRAM4_DATA Process p_supervisor;
-SRAM4_DATA static bool main_visible = false, vol_visible = false;
 SRAM4_DATA bool overlay_visible = false;
-SRAM4_DATA static kernel_time vol_tout;
+SRAM4_DATA static WidgetAnimationList wl;
 
 extern Condition scr_vsync;
 
 static void *supervisor_thread(void *p);
 
-
+ButtonWidget rw_test, rw_test2;
+ImageButtonWidget imb_bright_up, imb_bright_down;
+GridWidget scr_test;
+LabelWidget lab_caption;
+KeyboardWidget kw;
 static unsigned int scr_alpha = 0;
-
-static void handle_visible_change()
-{
-    if(main_visible || vol_visible)
-    {
-        overlay_visible = true;
-    }
-    else
-    {
-        overlay_visible = false;
-    }
-}
 
 void init_supervisor()
 {
@@ -52,12 +41,10 @@ void init_supervisor()
         p_supervisor.open_files[i] = nullptr;
     p_supervisor.screen_h = 480;
     p_supervisor.screen_w = 640;
-    p_supervisor.screen_pf = GK_PIXELFORMAT_L8;
     p_supervisor.gamepad_is_joystick = false;
     p_supervisor.gamepad_is_keyboard = true;
     p_supervisor.gamepad_to_scancode[GK_KEYVOLUP] = GK_SCANCODE_VOLUMEUP;
     p_supervisor.gamepad_to_scancode[GK_KEYVOLDOWN] = GK_SCANCODE_VOLUMEDOWN;
-    p_supervisor.gamepad_to_scancode[GK_KEYA] = GK_SCANCODE_RETURN;
     
     auto t = Thread::Create("supervisor_main", supervisor_thread, nullptr, true, 2,
         p_supervisor, PreferM7);
@@ -116,6 +103,18 @@ bool anim_showhide_overlay(Widget *wdg, void *p, unsigned long long int t)
     return false;
 }
 
+void imb_brightness_click(Widget *w, coord_t x, coord_t y)
+{
+    if(w == &imb_bright_down)
+    {
+        screen_set_brightness(screen_get_brightness() - 10);
+    }
+    else
+    {
+        screen_set_brightness(screen_get_brightness() + 10);
+    }
+}
+
 void kbd_click_up(Widget *w, coord_t x, coord_t y, int key)
 {
     focus_process->events.Push({ .type = Event::KeyUp, .key = (unsigned short)key });
@@ -126,139 +125,83 @@ void kbd_click_down(Widget *w, coord_t x, coord_t y, int key)
     focus_process->events.Push({ .type = Event::KeyDown, .key = (unsigned short)key });
 }
 
-static void scr_anim_set_y_cb(void *var, int32_t v)
-{
-    klog("cb: %d\n", v);
-    lv_obj_set_y((lv_obj_t *)var, v);
-    if(v >= 480 && main_visible)
-    {
-        main_visible = false;
-        handle_visible_change();
-    }
-    else if(v < 480 && !main_visible)
-    {
-        main_visible = true;
-        handle_visible_change();
-    }
-}
-
-static void vol_anim_set_opa_cb(void *var, int32_t v)
-{
-    lv_obj_set_style_opa((lv_obj_t *)var, v, 0);
-    if(v == 0 && vol_visible)
-    {
-        vol_visible = false;
-        handle_visible_change();
-    }
-}
-
 void *supervisor_thread(void *p)
 {
-    lv_init();
+    rw_test.x = 32;
+    rw_test.w = 200;
+    rw_test.y = 64;
+    rw_test.h = 100;
+    rw_test.text = "Hi there";
+    rw_test.OnClick = test_onclick;
 
-    auto display = lv_gk_display_create();
-    lv_display_set_default(display);
+    rw_test2.x = 264;
+    rw_test2.w = 200;
+    rw_test2.y = 64;
+    rw_test2.h = 100;
+    rw_test2.text = "Eh?";
 
-    auto indev_k = lv_gk_kbd_create();
-    auto grp = lv_group_create();
-    lv_indev_set_group(indev_k, grp);
-    lv_group_set_wrap(grp, false);
+    imb_bright_down.x = 640-80-16-80-16;
+    imb_bright_down.w = 80;
+    imb_bright_down.y = 64;
+    imb_bright_down.h = 80;
+    imb_bright_down.image = brightness_down;
+    imb_bright_down.img_w = 64;
+    imb_bright_down.img_h = 64;
+    imb_bright_down.OnClick = imb_brightness_click;
 
-    /* Set transparent screen */
-    lv_obj_set_style_bg_opa(lv_screen_active(), LV_OPA_TRANSP, LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(lv_layer_bottom(), LV_OPA_TRANSP, LV_PART_MAIN);
+    imb_bright_up.x = 640-80-16;
+    imb_bright_up.w = 80;
+    imb_bright_up.y = 64;
+    imb_bright_up.h = 80;
+    imb_bright_up.image = brightness_up;
+    imb_bright_up.img_w = 64;
+    imb_bright_up.img_h = 64;
+    imb_bright_up.OnClick = imb_brightness_click;
 
-    /* Remove scroll bars */
-    lv_obj_set_scrollbar_mode(lv_screen_active(), LV_SCROLLBAR_MODE_OFF);
+    lab_caption.x = 16;
+    lab_caption.y = 16;
+    lab_caption.w = 640 - 32;
+    lab_caption.h = 32;
+    lab_caption.text = "GK";
 
-    auto scr = lv_obj_create(lv_screen_active());
-    lv_obj_set_size(scr, LV_PCT(100), 300);
-    lv_obj_set_style_bg_color(scr, lv_color_make(255, 0, 0), 0);
-    lv_obj_set_style_bg_opa(scr, LV_OPA_80, 0);
-    lv_obj_set_y(scr, 480);
+    scr_test.x = 0;
+    scr_test.y = 240;
+    scr_test.w = 640;
+    scr_test.h = 240;
 
-    /* keyboard test */
-    [[maybe_unused]] auto kbd = lv_keyboard_create(scr);
-    lv_group_add_obj(grp, kbd);
+    RectangleWidget rw;
+    rw.x = 0;
+    rw.y = 0;
+    rw.w = fb_w;
+    rw.h = fb_h/2;
+    rw.bg_inactive_color = 0x87;
+    rw.border_width = 0;
 
-    static lv_style_t style_bg;
-    lv_style_init(&style_bg);
-    lv_style_set_pad_all(&style_bg, 2);
-    lv_style_set_pad_gap(&style_bg, 0);
-    lv_style_set_radius(&style_bg, 0);
-    lv_style_set_border_width(&style_bg, 0);
-    lv_style_set_outline_width(&style_bg, 0);
-    lv_style_set_bg_opa(&style_bg, LV_OPA_0);
+    scr_test.AddChild(rw);
+    //scr_test.AddChildOnGrid(rw_test);
+    //scr_test.AddChildOnGrid(rw_test2);
+    //scr_test.AddChildOnGrid(imb_bright_down);
+    //scr_test.AddChildOnGrid(imb_bright_up);
+    //scr_test.AddChild(lab_caption);
 
-    static lv_style_t style_btn;
-    lv_style_init(&style_btn);
-    lv_style_set_radius(&style_btn, 0);
-    lv_style_set_border_width(&style_btn, 1);
-    lv_style_set_border_opa(&style_btn, LV_OPA_100);
-    lv_style_set_border_color(&style_btn, lv_color_black());
-    lv_style_set_border_side(&style_btn, LV_BORDER_SIDE_INTERNAL);
-    lv_style_set_bg_opa(&style_btn, LV_OPA_50);
-    lv_style_set_radius(&style_btn, 0);
+    kw.x = (640 - kw.w) / 2;
+    kw.y = 8;
+    kw.OnKeyboardButtonClick = kbd_click_up;
+    kw.OnKeyboardButtonClickBegin = kbd_click_down;
+    scr_test.AddChildOnGrid(kw);
 
-    static lv_style_t style_btn_focus;
-    lv_style_init(&style_btn_focus);
-    lv_style_set_border_color(&style_btn_focus, lv_color_white());
-
-    lv_obj_add_style(kbd, &style_bg, 0);
-    lv_obj_add_style(kbd, &style_bg, LV_STATE_FOCUSED);
-    lv_obj_add_style(kbd, &style_bg, LV_STATE_FOCUS_KEY);
-    lv_obj_add_style(kbd, &style_btn, LV_PART_ITEMS);
-    lv_obj_add_style(kbd, &style_btn, LV_PART_ITEMS | LV_STATE_CHECKED);
-    lv_obj_add_style(kbd, &style_btn_focus, LV_PART_ITEMS | LV_STATE_FOCUSED);
-    lv_obj_add_style(kbd, &style_btn_focus, LV_PART_ITEMS | LV_STATE_FOCUS_KEY);
-    lv_obj_add_style(kbd, &style_btn_focus, LV_PART_ITEMS | LV_STATE_CHECKED | LV_STATE_FOCUSED);
-    lv_obj_add_style(kbd, &style_btn_focus, LV_PART_ITEMS | LV_STATE_CHECKED | LV_STATE_FOCUS_KEY);
-
-    /* volume control */
-    auto vctrl = lv_obj_create(lv_screen_active());
-    lv_obj_set_size(vctrl, LV_PCT(10), LV_PCT(30));
-    lv_obj_set_style_bg_color(vctrl, lv_color_make(255, 0, 0), 0);
-    lv_obj_set_style_bg_opa(vctrl, LV_OPA_80, 0);
-    lv_obj_set_pos(vctrl, 560, 60);
-    lv_obj_set_style_opa(vctrl, LV_OPA_0, 0);
-    lv_obj_set_scrollbar_mode(vctrl, LV_SCROLLBAR_MODE_OFF);
-
-    auto vol = lv_bar_create(vctrl);
-    lv_bar_set_range(vol, 0, 100);
-    lv_bar_set_value(vol, sound_get_volume(), LV_ANIM_OFF);
-    lv_obj_set_size(vol, 32, 100);
-    lv_obj_set_align(vol, LV_ALIGN_CENTER);
-
+    Widget *cur_scr = &scr_test;
 
     // process messages
     while(true)
     {
         Event e;
-        [[maybe_unused]] bool do_update = false;
-        auto thandler_delay = kernel_time::from_ms(lv_timer_handler());
-        klog("supervisor: thandler_delay: %llu\n", thandler_delay.to_us());
-
-        auto now = clock_cur();
-        auto v_delay = (vol_tout.is_valid() && vol_tout > now) ? (vol_tout - now) : kernel_time();
-
-        if(thandler_delay.is_valid() && v_delay.is_valid() && (v_delay < thandler_delay))
-            thandler_delay = v_delay;        
-
-        bool has_event = thandler_delay.is_valid() ? p_supervisor.events.Pop(&e, thandler_delay) :
-            p_supervisor.events.TryPop(&e);
-
-        if(vol_tout.is_valid() && clock_cur() >= vol_tout)
+        bool do_update = false;
+        bool has_event = p_supervisor.events.Pop(&e,
+            HasAnimations(wl) ? kernel_time::from_ms(1000ULL / 60ULL) : kernel_time());
+        if(RunAnimations(wl, clock_cur_ms()))
         {
-            vol_tout.invalidate();
-
-            lv_anim_t a;
-            lv_anim_init(&a);
-            lv_anim_set_var(&a, vctrl);
-            lv_anim_set_duration(&a, 500);
-            lv_anim_set_exec_cb(&a, vol_anim_set_opa_cb);
-            lv_anim_set_values(&a, LV_OPA_100, LV_OPA_0);
-            lv_anim_set_path_cb(&a, lv_anim_path_linear);
-            lv_anim_start(&a);
+            do_update = true;
         }
 
         if(has_event)
@@ -270,28 +213,13 @@ void *supervisor_thread(void *p)
                     {
                         case GK_SCANCODE_MENU:
                         {
-                            if(main_visible)
+                            if(overlay_visible)
                             {
-                                lv_anim_t a;
-                                lv_anim_init(&a);
-                                lv_anim_set_var(&a, scr);
-                                lv_anim_set_duration(&a, 500);
-                                lv_anim_set_exec_cb(&a, scr_anim_set_y_cb);
-                                lv_anim_set_values(&a, lv_obj_get_y(scr), 480);
-                                lv_anim_set_path_cb(&a, lv_anim_path_linear);
-                                lv_anim_start(&a);
-
+                                AddAnimation(wl, clock_cur_ms(), anim_showhide_overlay, &scr_test, (void*)0);
                             }
                             else
                             {
-                                lv_anim_t a;
-                                lv_anim_init(&a);
-                                lv_anim_set_var(&a, scr);
-                                lv_anim_set_duration(&a, 500);
-                                lv_anim_set_exec_cb(&a, scr_anim_set_y_cb);
-                                lv_anim_set_values(&a,  lv_obj_get_y(scr), 480 - lv_obj_get_height(scr));
-                                lv_anim_set_path_cb(&a, lv_anim_path_linear);
-                                lv_anim_start(&a);
+                                AddAnimation(wl, clock_cur_ms(), anim_showhide_overlay, &scr_test, (void*)1);
                             }
                             /*overlay_visible = !overlay_visible;
                             if(overlay_visible)
@@ -313,7 +241,13 @@ void *supervisor_thread(void *p)
                         case GK_SCANCODE_LCTRL:
                         case GK_SCANCODE_RETURN:
                         {
-                            lv_gk_input_push_event(e);
+                            auto ck = e.key;
+                            if(ck == GK_SCANCODE_A || ck == GK_SCANCODE_LCTRL)
+                            {
+                                ck = GK_SCANCODE_RETURN;
+                            }
+                            cur_scr->KeyPressDown(ck);
+                            do_update = true;
                         }
                         break;
 
@@ -324,12 +258,6 @@ void *supervisor_thread(void *p)
                             sound_set_volume(sound_get_volume() + 10);
 
                             klog("supervisor: sound set to %d\n", sound_get_volume());
-
-                            lv_bar_set_value(vol, sound_get_volume(), LV_ANIM_OFF);
-                            lv_obj_set_style_opa(vctrl, LV_OPA_100, 0);
-                            vol_visible = true;
-                            vol_tout = clock_cur() + kernel_time::from_ms(2000);
-                            handle_visible_change();
                             break;
                     }
                     break;
@@ -345,7 +273,13 @@ void *supervisor_thread(void *p)
                         case GK_SCANCODE_LCTRL:
                         case GK_SCANCODE_RETURN:
                         {
-                            lv_gk_input_push_event(e);
+                            auto ck = e.key;
+                            if(ck == GK_SCANCODE_A || ck == GK_SCANCODE_LCTRL)
+                            {
+                                ck = GK_SCANCODE_RETURN;
+                            }
+                            cur_scr->KeyPressUp(ck);
+                            do_update = true;
                         }
                     }
                     break;
@@ -362,18 +296,17 @@ void *supervisor_thread(void *p)
                             (" (" + std::to_string(focus_process->screen_w) + "x" +
                             std::to_string(focus_process->screen_h) + ")") : "";
 
-                        //lab_caption.text = capt + scr_capt;
+                        lab_caption.text = capt + scr_capt;
                         do_update = true;
                     }
                     break;
 
                 default:
-                    lv_gk_input_push_event(e);
+                    // ignore
                     break;
             }
         }
 
-/*
         if(overlay_visible && do_update)
         {
             auto fb = (color_t *)screen_get_overlay_frame_buffer();
@@ -386,13 +319,11 @@ void *supervisor_thread(void *p)
                 }
             }
 
-            //cur_scr->Update();
-            //screen_flip_overlay(true, scr_alpha);
-            //scr_vsync.Wait();
+            cur_scr->Update();
+            screen_flip_overlay(nullptr, true, scr_alpha);
+            scr_vsync.Wait();
         }
-*/    
     }
-
 
     return nullptr;
 }
