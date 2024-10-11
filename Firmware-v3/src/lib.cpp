da26806cb04f282a0f51dfc32f81a285e6447b0f
John Cronin <jncronin@tysos.org>
2024-10-11 13:56:08 +0100

    readd some syscalls to let libz work

--- Firmware-v3/src/lib.cpp
+++ Firmware-v3/src/lib.cpp
@@ -13,15 +13,10 @@ int _open(const char *pathname, int flags, mode_t mode)
 
 int _close(int file)
 {
-    BKPT();
-
-#if 0
     int ret = syscall_close1(file, &errno);
     if(ret != 0)
         return ret;
     return syscall_close2(file, &errno);
-#endif
-    return 0;
 }
 
 int _fstat(int file, void *st)
@@ -46,20 +41,12 @@ int _kill(int pid, int sig)
 
 int _lseek(int file, int offset, int whence)
 {
-    BKPT();
-#if 0
     return syscall_lseek(file, offset, whence, &errno);
-#endif
-    return 0;
 }
 
 int _read(int file, char *ptr, int len)
 {
-    BKPT();
-#if 0
     return syscall_read(file, ptr, len, &errno);
-#endif
-    return 0;
 }
 
 int _write(int file, char *buf, int nbytes)
