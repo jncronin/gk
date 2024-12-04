e1398c7522cef2a6ab9b862fb1e4415c95ed3e79
John Cronin <jncronin@tysos.org>
2024-12-04 17:26:30 +0000

    reenable touch panel

--- Firmware-v3/inc/gk_conf.h
+++ Firmware-v3/inc/gk_conf.h
@@ -8,7 +8,7 @@
 #define GK_ENABLE_USB_MASS_STORAGE  1
 #define GK_ENABLE_LWEXT4_WRITE      0
 #define GK_ENABLE_TEST_THREADS      0
-#define GK_ENABLE_TOUCH             0
+#define GK_ENABLE_TOUCH             1
 #define GK_USE_IRQ_PRIORITIES       0
 #define GK_DUAL_CORE                0
 #define GK_USE_CACHE                1
