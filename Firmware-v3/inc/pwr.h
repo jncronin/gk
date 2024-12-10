097204dd9bb053b1a9350eb0632d4d22bfce8972
John Cronin <jncronin@tysos.org>
2024-12-10 18:43:16 +0000

    add battery monitoring

--- Firmware-v3/inc/pwr.h
+++ Firmware-v3/inc/pwr.h
@@ -6,4 +6,21 @@ int pwr_set_vos_high();
 double pwr_get_vdd();
 void *pwr_monitor_thread(void *p);
 
+struct pwr_status
+{
+    double vdd;
+    double vbat;
+    double state_of_charge;
+    double charge_rate;
+    double time_until_full_empty;
+    bool is_charging;
+    bool is_full;
+    bool battery_present;
+    bool vreg_overtemperature;
+    bool vreg_undervoltage;
+    bool vreg_pgood;
+};
+
+void pwr_get_status(pwr_status *stat);
+
 #endif
