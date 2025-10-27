b15ae6a8a730db39296c39cb5e050bd929aa995e
John Cronin <jncronin@tysos.org>
2025-10-27 15:33:05 +0000

    switch to orange led (same as gk BTNLED_R)

--- Firmware-v4/fsbl-m/src/main.cpp
+++ Firmware-v4/fsbl-m/src/main.cpp
@@ -9,13 +9,13 @@ static const constexpr pin EV_ORANGE    { GPIOJ, 6 };
 
 int main()
 {
-    EV_BLUE.set_as_output();
+    EV_ORANGE.set_as_output();
 
     while(1)
     {
-        EV_BLUE.set();
+        EV_ORANGE.set();
         for(int i = 0; i < 2500000; i++);
-        EV_BLUE.clear();
+        EV_ORANGE.clear();
         for(int i = 0; i < 2500000; i++);
     }
 
