2d3a2da5ee24b7a0da742fa0a90c385cfb800d10
John Cronin <jncronin@tysos.org>
2025-12-02 21:20:42 +0000

    add vmem_vaddr_to_paddr_quick

--- Firmware-v4/gkos/inc/vmem.h
+++ Firmware-v4/gkos/inc/vmem.h
@@ -14,6 +14,29 @@ int vmem_map(uintptr_t vaddr, uintptr_t paddr, bool user, bool write, bool exec,
 int vmem_map(const VMemBlock &vaddr, const PMemBlock &paddr, uintptr_t ttbr0 = ~0ULL, uintptr_t ttbr1 = ~0ULL);
 int vmem_unmap(const VMemBlock &vaddr, uintptr_t ttbr0 = ~0ULL, uintptr_t ttbr1 = ~0ULL);
 void vmem_invlpg(uintptr_t vaddr, uintptr_t ttbr);
+uintptr_t vmem_vaddr_to_paddr(uintptr_t vaddr, uintptr_t ttbr0 = ~0ULL, uintptr_t ttbr1 = ~0ULL);
+
+static inline uintptr_t vmem_vaddr_to_paddr_quick(uintptr_t vaddr)
+{
+    uint64_t daif;
+    uint64_t paddr;
+
+    __asm__ volatile(
+        "mrs %[daif], daif\n"
+        "msr daifset, 0xf\n"
+        "at s1e0r, %[vaddr]\n"
+        "mrs %[paddr], par_el1\n"
+        "msr daif, %[daif]\n"
+        :
+        [daif] "=r" (daif),
+        [paddr] "=r" (paddr)
+        :
+        [vaddr] "r" (vaddr)
+        :
+        "memory"
+    );
+    return paddr;
+}
 
 /* Quick clear/copy, assumes multiples of 64 bytes, 16 byte alignment */
 extern "C" void quick_clear_64(void *dest, size_t n = 65536);
