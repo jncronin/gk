bb4572fc7d01485ba309f04e7aaed85ffcceec13
John Cronin <jncronin@tysos.org>
2025-12-07 16:02:26 +0000

    remove unused config option

--- Firmware-v4/gkos/inc/gk_conf.h
+++ Firmware-v4/gkos/inc/gk_conf.h
@@ -1,13 +1,12 @@
 #ifndef GK_CONF_H
 #define GK_CONF_H
 
-#define GK_NUM_CORES                1
+#define GK_NUM_CORES                2
 
 #define GK_ENABLE_NETWORK           0
 #define GK_ENABLE_WIFI              0
 #define GK_ENABLE_USB               1
 #define GK_ENABLE_USB_MASS_STORAGE  1
-#define GK_ENABLE_LWEXT4_WRITE      0
 #define GK_ENABLE_TEST_THREADS      0
 #define GK_ENABLE_TOUCH             1
 #define GK_ENABLE_TILT              1
