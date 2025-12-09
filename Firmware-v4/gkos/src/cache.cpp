ef0d477a2df592ad928e6003108c1915f15141e8
John Cronin <jncronin@tysos.org>
2025-12-09 08:04:35 +0000

    comment out unaligned cache op check

--- Firmware-v4/gkos/src/cache.cpp
+++ Firmware-v4/gkos/src/cache.cpp
@@ -1,12 +1,23 @@
 #include "cache.h"
+#include "logger.h"
+
+#define DEBUG_UNALIGNED     0
+#define ERROR_UNALIGNED     0
 
 /* PoU = all cores see the same thing (i.e. as far as L2 cache)
     PoC = all cores and external agents (e.g. DMA) see the same thing (i.e. as far as main memory) */
 
 void InvalidateA35Cache(uintptr_t base, uintptr_t length, CacheType_t ctype, bool for_dma)
 {
+#if DEBUG_UNALIGNED || ERROR_UNALIGNED
     if((base & (CACHE_LINE_SIZE - 1)) || (length & (CACHE_LINE_SIZE - 1)))
+    {
+        klog("cache: inv: unaligned %llx:%llx\n", base, length);
+#if ERROR_UNALIGNED
         __asm__ volatile("brk #249\n" ::: "memory");
+#endif
+    }
+#endif
     auto end = base + length;
     end = (end + (CACHE_LINE_SIZE - 1)) & ~(CACHE_LINE_SIZE - 1);
     base &= ~(CACHE_LINE_SIZE - 1);
@@ -41,8 +52,15 @@ void InvalidateA35Cache(uintptr_t base, uintptr_t length, CacheType_t ctype, boo
 
 void CleanA35Cache(uintptr_t base, uintptr_t length, CacheType_t ctype, bool for_dma)
 {
+#if DEBUG_UNALIGNED || ERROR_UNALIGNED
     if((base & (CACHE_LINE_SIZE - 1)) || (length & (CACHE_LINE_SIZE - 1)))
+    {
+        klog("cache: clean: unaligned %llx:%llx\n", base, length);
+#if ERROR_UNALIGNED
         __asm__ volatile("brk #250\n" ::: "memory");
+#endif
+    }
+#endif
     auto end = base + length;
     end = (end + (CACHE_LINE_SIZE - 1)) & ~(CACHE_LINE_SIZE - 1);
     base &= ~(CACHE_LINE_SIZE - 1);
