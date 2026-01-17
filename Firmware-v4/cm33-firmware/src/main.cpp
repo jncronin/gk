4eeb4830bdd050c3d18fe03869c69f8920824832
John Cronin <jncronin@tysos.org>
2026-01-17 21:48:46 +0000

    move cm33 to sram1/2

--- Firmware-v4/cm33-firmware/src/main.cpp
+++ Firmware-v4/cm33-firmware/src/main.cpp
@@ -1,24 +1,8 @@
 #include <stm32mp2xx.h>
 #include "pins.h"
 
-static const constexpr pin EV_BLUE      { GPIOJ, 7 };
-static const constexpr pin EV_RED       { GPIOH, 4 };
-static const constexpr pin EV_GREEN     { GPIOD, 8 };
-static const constexpr pin EV_ORANGE    { GPIOJ, 6 };
-
 int main()
 {
-    EV_GREEN.set_as_output();
-
-    // say hi
-    for(int n = 0; n < 10; n++)
-    {
-        EV_GREEN.set();
-        for(int i = 0; i < 2500000; i++);
-        EV_GREEN.clear();
-        for(int i = 0; i < 2500000; i++);
-    }
-
     while(true)
     {
         __SEV();
