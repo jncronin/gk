afd399fa8c2eb107e9559144693f3d35b8a76efb
John Cronin <jncronin@tysos.org>
2026-01-18 16:17:25 +0000

    add debounced digital joystick outputs

--- Firmware-v4/cm33-firmware/src/main.cpp
+++ Firmware-v4/cm33-firmware/src/main.cpp
@@ -11,6 +11,7 @@ const pin BTN_MCU_VOLDOWN { GPIOJ, 0 };
 
 unsigned int keystate = 0;
 uint32_t adc_vals[4];
+int16_t joy_a_x, joy_a_y, joy_b_x, joy_b_y;
 
 unsigned int ioexp_keystate = 0;
 
@@ -25,6 +26,28 @@ class ioexp_pin
                 return true;
             else
                 return false;
+        }
+};
+
+class joy_pin
+{
+    public:
+        bool positive;
+        int16_t *v;
+
+        joy_pin(int16_t *_v, bool pos) : positive(pos), v(_v) {}
+        bool value() const
+        {
+            // pretend to be an active low output
+
+            if(positive)
+            {
+                return *v < 8000;
+            }
+            else
+            {
+                return *v > -8000;
+            }
         }
 };
 
@@ -44,6 +67,15 @@ const ioexp_pin BTN_MCU_JOY_A(1U << 12);
 const ioexp_pin BTN_MCU_JOY_B(1U << 13);
 const ioexp_pin BTN_MCU_MENU(1U << 14);
 
+const joy_pin JOY_A_LEFT(&joy_a_x, false);
+const joy_pin JOY_A_RIGHT(&joy_a_x, true);
+const joy_pin JOY_A_UP(&joy_a_y, true);
+const joy_pin JOY_A_DOWN(&joy_a_y, false);
+const joy_pin JOY_B_LEFT(&joy_b_x, false);
+const joy_pin JOY_B_RIGHT(&joy_b_x, true);
+const joy_pin JOY_B_UP(&joy_b_y, true);
+const joy_pin JOY_B_DOWN(&joy_b_y, false);
+
 Debounce db_VOLUP(BTN_MCU_VOLUP, 1U << GK_KEYVOLUP);
 Debounce db_VOLDOWN(BTN_MCU_VOLDOWN, 1U << GK_KEYVOLDOWN);
 Debounce db_A(BTN_MCU_A, 1U << GK_KEYA);
@@ -61,6 +93,14 @@ Debounce db_RT(BTN_MCU_RT, 1U << GK_KEYRT);
 Debounce db_JOYBTNA(BTN_MCU_JOY_A, 1U << GK_KEYJOY);
 Debounce db_JOYBTNB(BTN_MCU_JOY_B, 1U << GK_KEYJOYB);
 Debounce db_MENU(BTN_MCU_MENU, 1U << GK_KEYMENU);
+Debounce db_JOY_A_LEFT(JOY_A_LEFT, 1U << GK_KEYJOYDIGILEFT);
+Debounce db_JOY_A_RIGHT(JOY_A_RIGHT, 1U << GK_KEYJOYDIGIRIGHT);
+Debounce db_JOY_A_UP(JOY_A_UP, 1U << GK_KEYJOYDIGIUP);
+Debounce db_JOY_A_DOWN(JOY_A_DOWN, 1U << GK_KEYJOYDIGIDOWN);
+Debounce db_JOY_B_LEFT(JOY_B_LEFT, 1U << GK_KEYJOYBDIGILEFT);
+Debounce db_JOY_B_RIGHT(JOY_B_RIGHT, 1U << GK_KEYJOYBDIGIRIGHT);
+Debounce db_JOY_B_UP(JOY_B_UP, 1U << GK_KEYJOYBDIGIUP);
+Debounce db_JOY_B_DOWN(JOY_B_DOWN, 1U << GK_KEYJOYBDIGIDOWN);
 
 int main()
 {
@@ -110,6 +150,41 @@ template <class T> void db_tick(T &db)
     }
 } 
 
+static int16_t joy_scale(uint32_t input, bool invert)
+{
+    int32_t i_input = (int32_t)input;
+    i_input -= 8192;
+    if(invert) i_input = -i_input;
+    i_input *= 4;
+
+    if(i_input < -32768) i_input = -32768;
+    if(i_input > 32767) i_input = 32767;
+
+    return (int16_t)i_input;
+}
+
+static void joystick_tick()
+{
+    /* map joystick axes from raw adc to something interpretable by SDL and
+        others
+
+        JOY A X : adc_vals[0] left = 0, right = 16k
+        JOY A Y : adc_vals[1] up = 16k, down = 0
+        JOY B X : adc_vals[3] left = 0, right = 16k (actually about 12k - problem with switch itself)
+        JOY B Y : adc_vals[2] up = 0, down = 16k
+
+        scale to:
+            X: left = -32k, right = +32k
+            Y: down = -32k, up = +32k
+
+        Add dead zone in the middle of 8k (after scaling), then pass through a debouncer for digital inputs */
+    
+    joy_a_x = joy_scale(adc_vals[0], false);
+    joy_a_y = joy_scale(adc_vals[1], false);
+    joy_b_x = joy_scale(adc_vals[3], false);
+    joy_b_y = joy_scale(adc_vals[2], true);    
+}
+
 static void tick()
 {
     uint8_t ioexp_vals[2];
@@ -119,6 +194,8 @@ static void tick()
         ioexp_keystate = (unsigned int)ioexp_vals[0] |
             (((unsigned int)ioexp_vals[1]) << 8);
     }
+
+    joystick_tick();
 
     db_tick(db_VOLUP);
     db_tick(db_VOLDOWN);
@@ -136,7 +213,15 @@ static void tick()
     db_tick(db_RT);
     db_tick(db_JOYBTNA);
     db_tick(db_JOYBTNB);
-    db_tick(db_MENU);    
+    db_tick(db_MENU);
+    db_tick(db_JOY_A_LEFT);
+    db_tick(db_JOY_A_RIGHT);
+    db_tick(db_JOY_A_UP);
+    db_tick(db_JOY_A_DOWN);
+    db_tick(db_JOY_B_LEFT);
+    db_tick(db_JOY_B_RIGHT);
+    db_tick(db_JOY_B_UP);
+    db_tick(db_JOY_B_DOWN);
 }
 
 extern "C" void TIM6_IRQHandler()
