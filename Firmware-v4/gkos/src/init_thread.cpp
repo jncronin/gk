685e19e0cf9ee81d804e1c5d6d9926f33f1f87e2
John Cronin <jncronin@tysos.org>
2026-01-19 16:38:03 +0000

    add gkmenu keybindings

--- Firmware-v4/gkos/src/init_thread.cpp
+++ Firmware-v4/gkos/src/init_thread.cpp
@@ -5,6 +5,7 @@
 #include "elf.h"
 #include "usb.h"
 #include "fs_provision.h"
+#include "_gk_scancodes.h"
 #include <fcntl.h>
 
 PProcess p_test;
@@ -33,11 +34,24 @@ void *init_thread(void *)
 
     p_test->env.cwd = "/gkmenu-0.1.1-gk";
     p_test->env.args.clear();
-    p_test->env.args.push_back("Doom");
+    //p_test->env.args.push_back("Doom");
 
     p_test->screen.screen_pf = GK_PIXELFORMAT_RGB565;
     p_test->screen.screen_w = 640;
     p_test->screen.screen_h = 480;
+
+    // gkmenu keymap
+    memset(&p_test->keymap, 0, sizeof(p_test->keymap));
+    p_test->keymap.gamepad_to_scancode[GK_KEYA] = GK_SCANCODE_RETURN;
+    p_test->keymap.gamepad_to_scancode[GK_KEYB] = GK_SCANCODE_ESCAPE;
+    p_test->keymap.gamepad_to_scancode[GK_KEYJOYDIGILEFT] = GK_SCANCODE_AUDIOPREV;
+    p_test->keymap.gamepad_to_scancode[GK_KEYJOYDIGIRIGHT] = GK_SCANCODE_AUDIONEXT;
+    p_test->keymap.gamepad_to_scancode[GK_KEYJOYDIGIUP] = GK_SCANCODE_AUDIOPREV;
+    p_test->keymap.gamepad_to_scancode[GK_KEYJOYDIGIDOWN] = GK_SCANCODE_AUDIONEXT;
+    p_test->keymap.gamepad_to_scancode[GK_KEYLEFT] = GK_SCANCODE_AUDIOPREV;
+    p_test->keymap.gamepad_to_scancode[GK_KEYRIGHT] = GK_SCANCODE_AUDIONEXT;
+    p_test->keymap.gamepad_to_scancode[GK_KEYUP] = GK_SCANCODE_AUDIOPREV;
+    p_test->keymap.gamepad_to_scancode[GK_KEYDOWN] = GK_SCANCODE_AUDIONEXT;
 
     if(ret == 0)
     {
