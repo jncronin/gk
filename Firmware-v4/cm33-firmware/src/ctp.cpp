796e89db31a29cc39151bc1c3c173f8f6ce4dba6
John Cronin <jncronin@tysos.org>
2026-02-25 13:44:02 +0000

    remove unused code

--- Firmware-v4/cm33-firmware/src/ctp.cpp
+++ Firmware-v4/cm33-firmware/src/ctp.cpp
@@ -33,27 +33,11 @@ static const constexpr uint8_t ctp_addr = 0x40;
 #define print_info(...) 
 static bool ctp_is_init = false;
 
-static uint32_t id_sign[MAX_CONTACTS+1] = {0};
-static uint8_t id_state_flag[MAX_CONTACTS+1] = {0};
-static uint8_t id_state_old_flag[MAX_CONTACTS+1] = {0};
-static uint16_t x_old[MAX_CONTACTS+1] = {0};
-static uint16_t y_old[MAX_CONTACTS+1] = {0};
-static uint16_t x_new = 0;
-static uint16_t y_new = 0;
-
 static std::atomic<bool> ctp_enabled = false;
 
 static void msleep(unsigned int ms)
 {
 	vTaskDelay(pdMS_TO_TICKS(ms));
-}
-
-static inline unsigned int join_bytes(char a, char b)
-{
-	unsigned int ab = 0;
-	ab = ab | a;
-	ab = ab << 8 | b;
-	return ab;
 }
 
 static int gsl_ts_write(int addr, char *pdata, unsigned int datalen)
@@ -182,79 +166,6 @@ static void gsl_reset()
 	startup_chip();
 }
 
-static void 
-record_point(uint16_t x, uint16_t y , unsigned int id)
-{
-	uint16_t x_err =0;
-	uint16_t y_err =0;
-
-	id_sign[id]=id_sign[id]+1;
-	
-	if(id_sign[id]==1){
-		x_old[id]=x;
-		y_old[id]=y;
-	}
-
-	x = (x_old[id] + x)/2;
-	y = (y_old[id] + y)/2;
-		
-	if(x>x_old[id]){
-		x_err=x -x_old[id];
-	}
-	else{
-		x_err=x_old[id]-x;
-	}
-
-	if(y>y_old[id]){
-		y_err=y -y_old[id];
-	}
-	else{
-		y_err=y_old[id]-y;
-	}
-
-	if( (x_err > 3 && y_err > 1) || (x_err > 1 && y_err > 3) ){
-		x_new = x;     x_old[id] = x;
-		y_new = y;     y_old[id] = y;
-	}
-	else{
-		if(x_err > 3){
-			x_new = x;     x_old[id] = x;
-		}
-		else
-			x_new = x_old[id];
-		if(y_err> 3){
-			y_new = y;     y_old[id] = y;
-		}
-		else
-			y_new = y_old[id];
-	}
-}
-
-static void
-report_data(
-	unsigned short x,
-	unsigned short y,
-	unsigned char pressure,
-	unsigned char id
-)
-{
-	unsigned short temp;
-	temp = x;
-	x = y;
-	y = temp;	
-	if(x>=SCREEN_MAX_X||y>=SCREEN_MAX_Y)
-		return;
-	klog("x=%d, y=%d, id=%d\n", x, y, id);
-
-#if 0
-	input_report_abs(ts.dev, ABS_MT_TRACKING_ID, id);
-	input_report_abs(ts.dev, ABS_MT_POSITION_X, x);
-	input_report_abs(ts.dev, ABS_MT_POSITION_Y, y);
-	input_report_abs(ts.dev, ABS_MT_TOUCH_MAJOR, pressure);
-	input_mt_sync(ts.dev);
-#endif
-}
-
 struct ctp_point
 {
 	int x, y;
@@ -342,98 +253,6 @@ static void read_ctp()
 
 	//klog("ctp: ntouched: %u\n", ntouched);
 
-}
-
-[[maybe_unused]] static void
-gp_multi_touch_work(
-)
-{
-	int i,ret;
-	char touched, id;
-	unsigned short x, y;
-	[[maybe_unused]] unsigned int pending;
-	[[maybe_unused]] int irq_state;
- 	char tp_data[(MULTI_TP_POINTS + 1)*4 ];
- 	
-	//print_info("WQ  gp_multi_touch_work.\n");
-
-#if ADJUST_CPU_FREQ
-	clockstatus_configure(CLOCK_STATUS_TOUCH,1);
-#endif
-
-	ret = gsl_ts_read(0x80, tp_data, sizeof(tp_data));
-	if( ret < 0) {
-		print_info("gp_tp_get_data fail,return %d\n",ret);
-		//gp_gpio_enable_irq(ts.client, 1);
-		return;
-	}
-
-	touched = (tp_data[0]< MULTI_TP_POINTS ? tp_data[0] : MULTI_TP_POINTS);
-#ifdef GSL_NOID_VERSION
-	cinfo.finger_num = touched;
-	print_info("tp-gsl  finger_num = %d\n",cinfo.finger_num);
-	for(i = 0; i < (touches < MAX_CONTACTS ? touches : MAX_CONTACTS); i ++)
-	{
-		cinfo.x[i] = join_bytes( ( ts->touch_data[ts->dd->x_index  + 4 * i + 1] & 0xf),
-				ts->touch_data[ts->dd->x_index + 4 * i]);
-		cinfo.x[i] = join_bytes(ts->touch_data[ts->dd->y_index + 4 * i + 1],
-				ts->touch_data[ts->dd->y_index + 4 * i ]);
-		print_info("tp-gsl  x = %d y = %d \n",cinfo.x[i],cinfo.y[i]);
-	}
-	gsl_alg_id_main(&cinfo);
-	touched = cinfo.finger_num;
-#endif
-	for(i=1;i<=MAX_CONTACTS;i++)
-	{
-		id_state_flag[i] = 0;		
-	}	
-	for (i = 0; i < touched; i++) {
-	#ifdef GSL_NOID_VERSION
-		id = cinfo.id[i];
-		x =  cinfo.x[i];
-		y =  cinfo.y[i];	
-	#else		
-		id = tp_data[4 *( i + 1) + 3] >> 4;
-		x = join_bytes(tp_data[4 *( i + 1) + 3] & 0xf,tp_data[4 *( i + 1) + 2]);
-		y = join_bytes(tp_data[4 *( i + 1) + 1],tp_data[4 *( i + 1) + 0]);		
-	#endif
-		if(1 <= id && id <= MAX_CONTACTS){
-			record_point(x, y, id);
-			report_data(x_new, y_new, 10, id);
-			id_state_flag[(unsigned int)id] = 1;
-		}
-	}
-	if (touched == 0) {
-#if 0
-		input_mt_sync(ts.dev);
-#endif
-	}
-	for(i=1;i<=MAX_CONTACTS;i++)
-	{	
-		if( (0 == touched) || ((0 != id_state_old_flag[i]) && (0 == id_state_flag[i])) )
-		{
-			id_sign[i]=0;
-		}
-		id_state_old_flag[i] = id_state_flag[i];		
-	}
-
-#if ADJUST_CPU_FREQ
-	if(touched == 0){
-		clockstatus_configure(CLOCK_STATUS_TOUCH,0);
-	}
-#endif
-#if 0
-	ts.prev_touched = touched;
-	input_sync(ts.dev);
-#endif
-
-	/* Clear interrupt flag */
-#if 0
-	pending = (1 << GPIO_PIN_NUMBER(ts.intIoIndex));
-	gpHalGpioSetIntPending(ts.intIoIndex, pending);
-
-	gp_gpio_enable_irq(ts.client, 1);
-#endif
 }
 
 static void check_mem_data(void)
