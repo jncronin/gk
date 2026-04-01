1eb1144b8687354cd5a5a32dbeccd5ff2a09749d
John Cronin <jncronin@tysos.org>
2026-04-01 18:13:11 +0100

    unit test for block_allocator

--- Firmware-v4/unit_tests/proc_vmem/main.cpp
+++ Firmware-v4/unit_tests/proc_vmem/main.cpp
@@ -1,65 +1,73 @@
 #include <cstdio>
 #include <assert.h>
-#include "proc_vmem.h"
+#include "block_allocator.h"
+
+using ba_t = BlockAllocator<int>;
+using MemBlock = ba_t::BlockAddress;
 
 MemBlock MakeMemBlock(uintptr_t base, uintptr_t length)
 {
 	MemBlock ret;
-	ret.b.base = base;
-	ret.b.length = length;
-	ret.b.valid = true;
-	printf("memblock: %p - %p created\n", (void*)ret.b.base, (void*)ret.b.end());
+	ret.start = base;
+	ret.length = length;
+	printf("memblock: %p - %p created\n", (void*)ret.start, (void*)ret.end());
 	return ret;
 }
 
 MemBlock MakeMemBlock(uintptr_t length)
 {
 	MemBlock ret;
-	ret.b.base = 0;
-	ret.b.length = length;
-	ret.b.valid = true;
+	ret.start = 0;
+	ret.length = length;
 	printf("memblock: any %p created\n", (void*)length);
 	return ret;
 }
 
-int dump(MemBlock& mb)
+int dump(const MemBlock& mb)
 {
-	printf("dump: %p - %p\n", (void*)mb.b.base, (void*)mb.b.end());
+	printf("dump: %p - %p\n", (void*)mb.start, (void*)mb.end());
 	return 0;
 }
 
 int main()
 {
-	MapVBlockAllocator a;
+	ba_t a({ 65536, 0x100000000 - 65536 });
 
 	// replicate a typical address space
-	assert(a.AllocFixed(MakeMemBlock(0x400000, 0x40000)).valid);
-	assert(a.AllocFixed(MakeMemBlock(0x440000, 0x10000)).valid);
-	assert(a.AllocFixed(MakeMemBlock(0x460000, 0x10000)).valid);
-	assert(a.AllocFixed(MakeMemBlock(0x450000, 0x10000)).valid);
+	assert(a.AllocFixed(MakeMemBlock(0x400000, 0x40000)) != a.end());
+	assert(a.AllocFixed(MakeMemBlock(0x440000, 0x10000)) != a.end());
+	assert(a.AllocFixed(MakeMemBlock(0x460000, 0x10000)) != a.end());
+	assert(a.AllocFixed(MakeMemBlock(0x450000, 0x10000)) != a.end());
 
-	a.Traverse(dump);
+	printf("\n");
+	for (const auto& mb : a)
+		dump(mb.first);
 
-	assert(a.AllocFixed(MakeMemBlock(0x410000, 0x8)).valid == false);
-	assert(a.IsAllocated(0x0).b.valid == false);
-	assert(a.IsAllocated(0x400000).b.valid);
-	assert(a.Dealloc(a.IsAllocated(0x440008)) == 0);
+	assert(a.AllocFixed(MakeMemBlock(0x410000, 0x8)) == a.end());
+	assert(a.IsAllocated(0x0) == a.end());
+	assert(a.IsAllocated(0x400000) != a.end());
+	assert(a.Dealloc(a.IsAllocated(0x440008)) != a.end());
 	
+	printf("\n");
+	for (const auto& mb : a)
+		dump(mb.first);
 
-	a.Traverse(dump);
+	assert(a.AllocAny(0x200000) != a.end());
+	assert(a.AllocAny(0x200000, 0, false) != a.end());
+	assert(a.AllocAny(0x200000) != a.end());
+	assert(a.AllocAny(0x200000, 0, false) != a.end());
+	assert(a.AllocAny(0x200000) != a.end());
+	assert(a.AllocAny(0x200000, 0, false) != a.end());
+	assert(a.AllocAny(0x200000) != a.end());
+	assert(a.AllocAny(0x200000, 0, false) != a.end());
 
-	assert(a.AllocAny(MakeMemBlock(0x200000)).valid);
-	assert(a.AllocAny(MakeMemBlock(0x200000), false).valid);
-	assert(a.AllocAny(MakeMemBlock(0x200000)).valid);
-	assert(a.AllocAny(MakeMemBlock(0x200000), false).valid);
-	assert(a.AllocAny(MakeMemBlock(0x200000)).valid);
-	assert(a.AllocAny(MakeMemBlock(0x200000), false).valid);
-	assert(a.AllocAny(MakeMemBlock(0x200000)).valid);
-	assert(a.AllocAny(MakeMemBlock(0x200000), false).valid);
+	assert(a.AllocAny((2ULL * 1024 * 1024 * 1024), false) != a.end());
 
-	assert(a.AllocAny(MakeMemBlock(2ULL * 1024 * 1024 * 1024), false).valid);
+	assert(a.AllocAny(0x200000, 0, true) != a.end());
 
-	a.Traverse(dump);
+	printf("\n");
+	for (const auto& mb : a)
+		dump(mb.first);
 
 	return 0;
 }
