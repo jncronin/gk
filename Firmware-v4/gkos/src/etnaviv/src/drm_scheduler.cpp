09bdd918fd10c0682d19a653ead97b014062f26f
John Cronin <jncronin@tysos.org>
2026-04-14 15:21:51 +0100

    signal completion even if job timed out

--- Firmware-v4/gkos/src/etnaviv/src/drm_scheduler.cpp
+++ Firmware-v4/gkos/src/etnaviv/src/drm_scheduler.cpp
@@ -151,14 +151,14 @@ void *drm_sched_worker(void *p)
 #if GPU_DEBUG > 1
             klog("drm_sched_worker: job %u completed\n", j->job_id);
 #endif
-            auto finish_signal = j->finished;
-            j = nullptr;        // release any locks prior to signalling out fence
-            finish_signal->Signal();
         }
         else
         {
             klog("drm_sched_worker: job %u timed out\n", j->job_id);
         }
+        auto finish_signal = j->finished;
+        j = nullptr;        // release any locks prior to signalling out fence
+        finish_signal->Signal();
     }
 }
 
